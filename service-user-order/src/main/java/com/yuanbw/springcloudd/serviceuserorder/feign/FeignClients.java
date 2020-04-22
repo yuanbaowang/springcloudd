11fa9ce1444cc7ed386aa6a616d45f827ec260e9
yuanbaowang <2637562166@qq.com>
2020-04-22 11:26:30 +0800

    order 中的fegin接口 添加注释

--- service-user-order/src/main/java/com/yuanbw/springcloudd/serviceuserorder/feign/FeignClients.java
+++ service-user-order/src/main/java/com/yuanbw/springcloudd/serviceuserorder/feign/FeignClients.java
@@ -4,6 +4,11 @@ import com.yuanbw.springcloudd.serviceuserinterface.user.User;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ *  feign指定了serrvice-user 客户端
+ *  如果其他客户端想要调用这个同一个接口
+ *  的话  把它放在接口工程 就不需要重新编写
+ */
 @FeignClient(name = "service-user")
 public interface FeignClients {
 
